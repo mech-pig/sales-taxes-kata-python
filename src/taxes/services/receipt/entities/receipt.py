7374c4d42789028e06c58ac6b017dc7a5dc7a577
mechpig <7295856+mech-pig@users.noreply.github.com>
2020-04-08 23:39:30 +0200

    refactor: improve naming of receipt module

--- src/taxes/services/receipt/entities/receipt.py
+++ src/taxes/services/receipt/entities/receipt.py
@@ -7,12 +7,7 @@ from taxes.services.receipt.entities import tax
 
 
 @dataclass(frozen=True)
-class Receipt:
-    items: List['ReceiptItem']
-
-
-@dataclass(frozen=True)
-class ReceiptItem:
+class ItemToInsert:
     description: str
     quantity: int
     unit_price_before_taxes: Decimal
@@ -20,42 +15,45 @@ class ReceiptItem:
 
 
 @dataclass(frozen=True)
-class FinalizedReceipt:
-    items: List['FinalizedReceiptItem']
+class Receipt:
+    items: List['ReceiptItem']
     taxes_due: Decimal
     total_due: Decimal
 
 
 @dataclass(frozen=True)
-class FinalizedReceiptItem:
+class ReceiptItem:
     description: str
     quantity: int
     subtotal_price_with_taxes: Decimal
 
 
 def empty():
-    return Receipt(items=[])
+    return Receipt(items=[], taxes_due=Decimal('0'), total_due=Decimal('0'))
 
 
-def finalize_item(finalized: FinalizedReceipt, item: ReceiptItem) -> FinalizedReceipt:
-    taxed_item = FinalizedReceiptItem(
-        description=item.description,
-        quantity=item.quantity,
-        subtotal_price_with_taxes=tax.apply(item.unit_price_before_taxes, item.taxes_to_apply),
+def insert_item(to_insert: ItemToInsert, receipt: Receipt) -> Receipt:
+    new_receipt_item = ReceiptItem(
+        description=to_insert.description,
+        quantity=to_insert.quantity,
+        subtotal_price_with_taxes=tax.apply(
+            to_insert.unit_price_before_taxes,
+            to_insert.taxes_to_apply,
+        ),
     )
 
-    item_taxes_due = taxed_item.subtotal_price_with_taxes - item.unit_price_before_taxes
+    item_taxes_due = new_receipt_item.subtotal_price_with_taxes - to_insert.unit_price_before_taxes
 
-    return FinalizedReceipt(
-        items=[*finalized.items, taxed_item],
-        taxes_due=finalized.taxes_due + item_taxes_due,
-        total_due=finalized.total_due + taxed_item.subtotal_price_with_taxes,
+    return Receipt(
+        items=[*receipt.items, new_receipt_item],
+        taxes_due=receipt.taxes_due + item_taxes_due,
+        total_due=receipt.total_due + new_receipt_item.subtotal_price_with_taxes,
     )
 
 
-def finalize(receipt: Receipt) -> FinalizedReceipt:
+def finalize(items_to_insert: List[ItemToInsert]) -> Receipt:
     return reduce(
-        finalize_item,
-        receipt.items,
-        FinalizedReceipt(items=[], taxes_due=Decimal('0'), total_due=Decimal('0')),
+        lambda receipt, to_insert: insert_item(to_insert, receipt),
+        items_to_insert,
+        empty(),
     )
