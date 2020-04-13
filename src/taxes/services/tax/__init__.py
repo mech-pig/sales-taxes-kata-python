8c98eee586d6538b28ff7dcf77a6d89249949de3
mechpig <7295856+mech-pig@users.noreply.github.com>
2020-04-13 04:07:53 +0200

    add: tax service entrypoint
    Closes #26

---
+++ src/taxes/services/tax/__init__.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Iterable, Protocol
+
+from taxes.services.basket.entities.article import Article
+from taxes.services.receipt.entities.receipt import ItemToInsert
+
+
+@dataclass
+class TaxService:
+    logger: 'Dependency.Logger'
+
+    def add_taxes(self, articles: Iterable[Article]) -> Iterable[ItemToInsert]:
+        self.logger.info('wip: no taxes will be added')
+        return [
+            ItemToInsert(
+                description=article.product.name,
+                quantity=article.quantity,
+                unit_price_before_taxes=article.product.unit_price,
+                taxes_to_apply=[],
+            ) for article in articles
+        ]
+
+
+class Dependency:
+    class Logger(Protocol):
+        def info(self, msg: str):  # pragma: no cover
+            ...
+
+        def debug(self, msg: str):  # pragma: no cover
+            ...
+
+
+def create(logger: Dependency.Logger):
+    logger.debug(f'instantiating tax service with logger={logger}')
+    return TaxService(logger=logger)
--- test/services/basket/test_service.py
+++ test/services/basket/test_service.py
@@ -21,6 +21,6 @@ def test_create_returns_service(make_dependencies_fixture):
 
 
 @create_basket_test_cases
-def test_create_receipt_returns_receipt(case, make_dependencies_fixture):
+def test_create_basket_returns_basket(case, make_dependencies_fixture):
     service = basket.create(**make_dependencies_fixture())
-    case.expected = service.create_basket(articles=case.articles)
+    assert case.expected == service.create_basket(articles=case.articles)
