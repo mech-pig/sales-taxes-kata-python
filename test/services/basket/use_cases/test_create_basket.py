1aa23bcf4d960e4bd39d963cebb989cc3253d27f
mechpig <7295856+mech-pig@users.noreply.github.com>
2020-04-13 03:32:07 +0200

    add: basket service entrypoint
    Closes #25

--- test/services/basket/use_cases/test_create_basket.py
+++ test/services/basket/use_cases/test_create_basket.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from decimal import Decimal
+from typing import List
 from unittest.mock import Mock
 
 import pytest
@@ -7,80 +9,73 @@ from taxes.services.basket.entities import article, basket, product
 from taxes.services.basket.use_cases import create_basket
 
 
-basket_test_cases = pytest.mark.parametrize(
+@dataclass
+class CreateBasketTestCase:
+    articles: List[article.Article]
+    expected: basket.Basket
+
+
+TEST_CASES = {
+    'empty article list': CreateBasketTestCase(
+        articles=[],
+        expected=basket.empty()
+    ),
+    'list with single article': CreateBasketTestCase(
+        articles=[
+            article.create(product_name='A', product_unit_price=Decimal('1'), quantity=1),
+        ],
+        expected=basket.Basket(
+            articles={
+                product.create(name='A', unit_price=Decimal('1')): 1,
+            }
+        ),
+    ),
+    'multiple products': CreateBasketTestCase(
+        articles=[
+            article.create(product_name='B', product_unit_price=Decimal('1'), quantity=2),
+            article.create(product_name='A', product_unit_price=Decimal('1'), quantity=3),
+            article.create(product_name='C', product_unit_price=Decimal('1'), quantity=5),
+        ],
+        expected=basket.Basket(
+            articles={
+                product.create(name='A', unit_price=Decimal('1')): 3,
+                product.create(name='B', unit_price=Decimal('1')): 2,
+                product.create(name='C', unit_price=Decimal('1')): 5,
+            }
+        ),
+    ),
+    'same product added multiple times': CreateBasketTestCase(
+        articles=[
+            article.create(product_name='B', product_unit_price=Decimal('1'), quantity=1),
+            article.create(product_name='A', product_unit_price=Decimal('1'), quantity=1),
+            article.create(product_name='B', product_unit_price=Decimal('1'), quantity=5),
+            article.create(product_name='B', product_unit_price=Decimal('1'), quantity=3),
+            article.create(product_name='A', product_unit_price=Decimal('1'), quantity=2),
+        ],
+        expected=basket.Basket(
+            articles={
+                product.create(name='A', unit_price=Decimal('1')): 3,
+                product.create(name='B', unit_price=Decimal('1')): 9,
+            }
+        ),
+    ),
+    'same product name, but different unit prices': CreateBasketTestCase(
+        articles=[
+            article.create(product_name='A', product_unit_price=Decimal('1'), quantity=2),
+            article.create(product_name='A', product_unit_price=Decimal('1.1'), quantity=3),
+        ],
+        expected=basket.Basket(
+            articles={
+                product.create(name='A', unit_price=Decimal('1')): 2,
+                product.create(name='A', unit_price=Decimal('1.1')): 3,
+            }
+        ),
+    ),
+}
+
+create_basket_test_cases = pytest.mark.parametrize(
     'case',
-    [
-        pytest.param(
-            {
-                'articles': [],
-                'expected': basket.empty(),
-            },
-            id='empty article list',
-        ),
-        pytest.param(
-            {
-                'articles': [
-                    article.create(product_name='A', product_unit_price=Decimal('1'), quantity=1),
-                ],
-                'expected': basket.Basket(
-                    articles={
-                        product.create(name='A', unit_price=Decimal('1')): 1,
-                    }
-                ),
-            },
-            id='list with single article',
-        ),
-        pytest.param(
-            {
-                'articles': [
-                    article.create(product_name='B', product_unit_price=Decimal('1'), quantity=2),
-                    article.create(product_name='A', product_unit_price=Decimal('1'), quantity=3),
-                    article.create(product_name='C', product_unit_price=Decimal('1'), quantity=5),
-                ],
-                'expected': basket.Basket(
-                    articles={
-                        product.create(name='A', unit_price=Decimal('1')): 3,
-                        product.create(name='B', unit_price=Decimal('1')): 2,
-                        product.create(name='C', unit_price=Decimal('1')): 5,
-                    }
-                ),
-            },
-            id='multiple products',
-        ),
-        pytest.param(
-            {
-                'articles': [
-                    article.create(product_name='B', product_unit_price=Decimal('1'), quantity=1),
-                    article.create(product_name='A', product_unit_price=Decimal('1'), quantity=1),
-                    article.create(product_name='B', product_unit_price=Decimal('1'), quantity=5),
-                    article.create(product_name='B', product_unit_price=Decimal('1'), quantity=3),
-                    article.create(product_name='A', product_unit_price=Decimal('1'), quantity=2),
-                ],
-                'expected': basket.Basket(
-                    articles={
-                        product.create(name='A', unit_price=Decimal('1')): 3,
-                        product.create(name='B', unit_price=Decimal('1')): 9,
-                    }
-                ),
-            },
-            id='same product added multiple times',
-        ),
-        pytest.param(
-            {
-                'articles': [
-                    article.create(product_name='A', product_unit_price=Decimal('1'), quantity=2),
-                    article.create(product_name='A', product_unit_price=Decimal('1.1'), quantity=3),
-                ],
-                'expected': basket.Basket(
-                    articles={
-                        product.create(name='A', unit_price=Decimal('1')): 2,
-                        product.create(name='A', unit_price=Decimal('1.1')): 3,
-                    }
-                ),
-            },
-            id='same product name, but different unit prices',
-        ),
-    ],
+    [pytest.param(case, id=id) for id, case in TEST_CASES.items()],
 )
 
 
@@ -101,25 +96,21 @@ def make_env_fixture(info):
     return build
 
 
-@basket_test_cases
+@create_basket_test_cases
 def test_create_returns_use_case(case):
-    articles = case['articles']
-    run = create_basket.create(articles=articles)
+    run = create_basket.create(articles=case.articles)
     assert isinstance(run, create_basket.CreateBasketUseCase)
 
 
-@basket_test_cases
+@create_basket_test_cases
 def test_running_create_basket_use_cases_returns_basket(case, make_env_fixture):
-    articles = case['articles']
-    run = create_basket.create(articles=articles)
+    run = create_basket.create(articles=case.articles)
     env = make_env_fixture()
     assert isinstance(run(env), basket.Basket)
 
 
-@basket_test_cases
+@create_basket_test_cases
 def test_returned_basket_contains_added_articles(case, make_env_fixture):
-    articles = case['articles']
-    expected = case['expected']
-    run = create_basket.create(articles=articles)
+    run = create_basket.create(articles=case.articles)
     env = make_env_fixture()
-    assert expected == run(env)
+    assert case.expected == run(env)
